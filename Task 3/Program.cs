0d065d70944e2fc8b9eae547ed3cc042f84d1b40
belevgen <belsky_evgeniy@mail.rum>
2022-06-27 00:31:55 +0600

    ДЗ Семинар 5_20.06.22_Задача 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным
+// элементов массива.
+
+int [] CreateAndFillArr(int large, int min, int max)
+{
+    int [] array = new int [large];
+    for (int iffil = 0; iffil < array.Length; iffil++)
+    {
+        array [iffil] = new Random ().Next(min, max + 1);
+    }
+    return array;
+}
+void printArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int FindDiferenceBetweenMinMax(int[] arr)
+{
+    int min = arr [0];
+    int max = arr [0];
+    int diferenc = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr [i] > max)
+        {
+            max = arr[i];
+        }
+        else
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    diferenc = max - min;
+    return diferenc;
+}
+
+int[] myArray = CreateAndFillArr(10, 0, 50);
+printArr(myArray);
+var dif = FindDiferenceBetweenMinMax(myArray);
+System.Console.WriteLine($"Разница между максимальным и минимальным числом = {dif}");
+
